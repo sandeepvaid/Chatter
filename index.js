e1fe5dc7758ca4b6a7d238f8d3e9d53020b8d51d
SANDEEP vaid <vaid77167@gmail.com>
2021-10-19 12:53:15 +0530

    set up ejs as template engine

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const port = 8000;
 //Use express router 
 app.use('/',require('./routes'));
 
+//Setup the view engine
+app.set('view engine','ejs');
+app.set('views','./views');
 
 app.listen(port,(err)=>{
 
